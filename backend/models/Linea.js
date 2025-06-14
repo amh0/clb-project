e49ad919f7735203a4f10a1aed83e31382ad56f0
dot <argani.amh@gmail.com>
2025-06-14 07:10:53 -0400

    Change Line schema from array of point id's to array of (lat,long) pairs
    Read queries would be faster this way because '.populate()' function isn't going to be used.

--- backend/models/Linea.js
+++ backend/models/Linea.js
@@ -3,14 +3,28 @@ const mongoose = require("mongoose");
 const LineaSchema = new mongoose.Schema({
   numero: {
     type: Number,
-    require: true,
+    required: true,
   },
   sindicato: {
     type: String,
-    require: false,
+    required: false,
   },
 
-  puntos: [{ type: mongoose.Schema.Types.ObjectId, ref: "Ubicacion" }],
+  puntos: [
+    {
+      lat: {
+        type: Number,
+        required: true,
+      },
+      long: {
+        type: Number,
+        required: true,
+      },
+    },
+  ],
 });
+
+// Speed up queries of lat long
+LineaSchema.index({ "puntos.lat": 1, "puntos.long": 1 });
 
 module.exports = mongoose.model("Linea", LineaSchema, "Lineas");
