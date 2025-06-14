109a5aea8aabfea34c877db8c8efe83af8eb47fa
dot <argani.amh@gmail.com>
2025-06-14 10:45:58 -0400

    Add end point to add and retrieve points

--- backend/controllers/points.js
+++ backend/controllers/points.js
@@ -12,4 +12,56 @@ async function getAll(req, res) {
   }
 }
 
-module.exports = { getAll };
+async function findOrCreatePoint(point) {
+  const { lat, lon } = point;
+  const resPoint = await Point.findOneAndUpdate(
+    { lat, lon },
+    {
+      $setOnInsert: {
+        lat,
+        lon,
+        location: {
+          type: "Point",
+          coordinates: [lon, lat],
+        },
+      },
+    },
+    { upsert: true, new: true }
+  );
+
+  return resPoint;
+}
+
+async function findClosestPoint(req, res) {
+  try {
+    const { lat, lon } = req.body;
+
+    if (typeof lat !== "number" || typeof lon !== "number") {
+      return errorResponse(res, 400, "Latitud y longitud deben ser números");
+    }
+
+    const closestPoint = await Point.findOne({
+      location: {
+        $near: {
+          $geometry: {
+            type: "Point",
+            coordinates: [lon, lat],
+          },
+          $maxDistance: 5000, // meters
+        },
+      },
+    });
+
+    if (!closestPoint) {
+      return errorResponse(res, 404, "No se encontró un punto cercano");
+    }
+
+    return successResponse(res, 200, "Punto más cercano encontrado", {
+      point: closestPoint,
+    });
+  } catch (err) {
+    console.error("Error en findClosestPoint", err);
+    return errorResponse(res, 500, "Error interno del servidor", err.message);
+  }
+}
+module.exports = { getAll, findOrCreatePoint, findClosestPoint };
