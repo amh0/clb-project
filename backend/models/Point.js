9446dc5b903ab968c5bbe698cb8655a73f1fe7bf
dot <argani.amh@gmail.com>
2025-06-14 10:46:53 -0400

    Add index to point schema, fix typos

--- backend/models/Point.js
+++ backend/models/Point.js
@@ -1,16 +1,13 @@
 const mongoose = require("mongoose");
 
-const PointsSchema = new mongoose.Schema({
-  desc: {
-    type: String,
-  },
+const PointSchema = new mongoose.Schema({
   lat: {
     type: Number,
-    require: true,
+    required: true,
   },
   lon: {
     type: Number,
-    require: true,
+    required: true,
   },
   location: {
     type: {
@@ -25,6 +22,7 @@ const PointsSchema = new mongoose.Schema({
   },
 });
 
-UbicacionSchema.index({ location: "2dsphere" });
+PointSchema.index({ lat: 1, lon: 1 }, { unique: true });
+PointSchema.index({ location: "2dsphere" });
 
-module.exports = mongoose.model("Point", PointsSchema, "Points");
+module.exports = mongoose.model("Point", PointSchema, "Points");
