214cd2b7b988dda7c61b664393b81f9405a6bbae
dot <argani.amh@gmail.com>
2025-07-22 07:04:46 -0400

    chore: clean log messages

--- backend/utils/geoPoints.js
+++ backend/utils/geoPoints.js
@@ -4,14 +4,10 @@ function transformCoordinates(coordinates) {
   if (Array.isArray(coordinates) && coordinates.length === 2) {
     return { lat: coordinates[1], lon: coordinates[0] };
   }
-  console.log(coordinates);
-  console.log("not returning");
   return null;
 }
 
 function transformGeoJSONPoints(points) {
-  console.log("this is the geoJSON func");
-  console.log(points);
   return points.map((point) => transformCoordinates(point.coordinates));
 }
 
